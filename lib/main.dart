47d5044cc3655833541811c66439a868edf10fc2
Richiedei78 <invest@sfip.de>
2025-05-25 10:21:29 +0200

    add flutter cli

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sfip_tasks/app_theme.dart';
 import 'package:sfip_tasks/home_page.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
