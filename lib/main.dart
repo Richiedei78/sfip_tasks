813772facea83310598aed52fdc1da02ba783c40
Richiedei78 <invest@sfip.de>
2025-05-25 17:47:33 +0200

    firebase settings added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sfip_tasks/home_page.dart';
 import 'package:sfip_tasks/statemanagement/theme_provider.dart';
 import 'package:sfip_tasks/screens/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,7 +29,12 @@ class MyApp extends StatelessWidget {
       title: 'SFIP',
       debugShowCheckedModeBanner: false,
       theme: themeProvider.isDarkMode ? ThemeData.dark() : ThemeData.light(),
-      home: const LoginScreen(),
+      home: _handleUserNavigation(), // 🔥 Login-Prüfung
     );
+  }
+
+  Widget _handleUserNavigation() {
+    User? user = FirebaseAuth.instance.currentUser;
+    return user == null ? const LoginScreen() : const MyHomePage(title: 'Dashboard');
   }
 }
