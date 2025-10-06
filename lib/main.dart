56a7c9eee8f1213f9869d1db8dd810728269afb3
Richiedei78 <invest@sfip.de>
2025-10-06 19:14:55 +0200

    restart todo app

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:sfip_tasks/home_page.dart';
-import 'package:sfip_tasks/statemanagement/theme_provider.dart';
-import 'package:sfip_tasks/screens/login_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
-      child: const MyApp(),
-    ),
-  );
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -23,18 +9,136 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final themeProvider = Provider.of<ThemeProvider>(context);
-
     return MaterialApp(
-      title: 'SFIP',
-      debugShowCheckedModeBanner: false,
-      theme: themeProvider.isDarkMode ? ThemeData.dark() : ThemeData.light(),
-      home: _handleUserNavigation(), // 🔥 Login-Prüfung
+      title: 'Todo App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const TodoListScreen(),
+    );
+  }
+}
+
+class TodoListScreen extends StatefulWidget {
+  const TodoListScreen({super.key});
+
+  @override
+  State<TodoListScreen> createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  final List<TodoItem> _todos = [];
+  final TextEditingController _controller = TextEditingController();
+
+  void _addTodo() {
+    if (_controller.text.isNotEmpty) {
+      setState(() {
+        _todos.add(TodoItem(title: _controller.text));
+        _controller.clear();
+      });
+    }
+  }
+
+  void _toggleTodo(int index) {
+    setState(() {
+      _todos[index].isCompleted = !_todos[index].isCompleted;
+    });
+  }
+
+  void _deleteTodo(int index) {
+    setState(() {
+      _todos.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meine Todos'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      hintText: 'Neues Todo hinzufügen',
+                      border: OutlineInputBorder(),
+                    ),
+                    onSubmitted: (_) => _addTodo(),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                IconButton(
+                  onPressed: _addTodo,
+                  icon: const Icon(Icons.add),
+                  style: IconButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    foregroundColor: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child:
+                _todos.isEmpty
+                    ? const Center(
+                      child: Text(
+                        'Keine Todos vorhanden.\nFüge dein erstes Todo hinzu!',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 16, color: Colors.grey),
+                      ),
+                    )
+                    : ListView.builder(
+                      itemCount: _todos.length,
+                      itemBuilder: (context, index) {
+                        final todo = _todos[index];
+                        return ListTile(
+                          leading: Checkbox(
+                            value: todo.isCompleted,
+                            onChanged: (_) => _toggleTodo(index),
+                          ),
+                          title: Text(
+                            todo.title,
+                            style: TextStyle(
+                              decoration:
+                                  todo.isCompleted
+                                      ? TextDecoration.lineThrough
+                                      : null,
+                              color: todo.isCompleted ? Colors.grey : null,
+                            ),
+                          ),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete, color: Colors.red),
+                            onPressed: () => _deleteTodo(index),
+                          ),
+                        );
+                      },
+                    ),
+          ),
+        ],
+      ),
     );
   }
 
-  Widget _handleUserNavigation() {
-    User? user = FirebaseAuth.instance.currentUser;
-    return user == null ? const LoginScreen() : const MyHomePage(title: 'Dashboard');
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
+}
+
+class TodoItem {
+  String title;
+  bool isCompleted;
+
+  TodoItem({required this.title, this.isCompleted = false});
 }
